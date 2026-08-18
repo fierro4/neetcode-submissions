f7df33c363805a8870f7f201b0ccf6d3bf9a2a7d
Priyanka <111179140+fierro4@users.noreply.github.com>
2026-08-18 16:02:58 +0530

    Add: permutation-string - submission-8

---
+++ Data Structures & Algorithms/permutation-string/submission-8.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+
+        int[] count1 = new int[26];
+        int[] count2 = new int[26];
+
+        for (char c : s1.toCharArray()) {
+            count1[c - 'a']++;
+        }
+
+        int l = 0;
+
+        for (int r = 0; r < s2.length(); r++) {
+            count2[s2.charAt(r) - 'a']++;
+
+            if (r - l + 1 > s1.length()) {
+                count2[s2.charAt(l) - 'a']--;
+                l++;
+            }
+
+            if (Arrays.equals(count1, count2)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
